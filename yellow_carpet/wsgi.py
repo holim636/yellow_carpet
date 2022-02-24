ee195c30bcb989462d3a9892e07bee9fe4af916d
holim636 <holim636@naver.com>
2022-02-24 10:12:43 +0900

    Add files via upload

---
+++ yellow_carpet/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for yellow_carpet project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yellow_carpet.settings')
+
+application = get_wsgi_application()
